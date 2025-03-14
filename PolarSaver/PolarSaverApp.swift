6df14c9c6fa0586afce63c9bf390cf1f53012968
Junho Choi <junhochoi13@gmail.com>
2025-03-14 10:06:55 +0000

    Initial Commit

---
+++ PolarSaver/PolarSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolarSaverApp.swift
+//  PolarSaver
+//
+//  Created by Junho Choi on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PolarSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PolarSaverTests/PolarSaverTests.swift
@@ -0,0 +1,17 @@
+//
+//  PolarSaverTests.swift
+//  PolarSaverTests
+//
+//  Created by Junho Choi on 14/03/2025.
+//
+
+import Testing
+@testable import PolarSaver
+
+struct PolarSaverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
